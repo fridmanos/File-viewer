14dbfa2e9454a5b47536a29e21da63d2298dbf69
Isak <isakfridman@protonmail.com>
2018-02-17 18:12:47 +0100

    Update compile.sh
    Fixed typo

--- src/compile.sh
+++ src/compile.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-#Don't know cmake so here's a simple bash script for compialation
+#Don't know cmake so here's a simple bash script for compiling
 g++ -c Directory.cpp -o Directory.o
 g++ Main.cpp Directory.o -o File-viewer
 
