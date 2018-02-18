bed39145abe76b6ef504bad4b8a641728e336272
Isak <isakfridman@protonmail.com>
2018-02-18 12:11:34 +0100

    Update compile.sh

--- src/compile.sh
+++ src/compile.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 #Don't know cmake so here's a simple bash script for compiling
 g++ -c Directory.cpp -o Directory.o
-g++ Main.cpp Directory.o -o File-viewer
-
+g++ -c Functions.cpp -o Functions.o
+g++ Functions.o Directory.o Main.cpp -o File-viewer
