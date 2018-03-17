19f38c2954d00a0de2fc07d83ce4f96ca05b0758
Isak <isakfridman@protonmail.com>
2018-03-17 20:40:56 +0100

    Update compile.sh
    Update bash script to specify c++ standard.

--- src/compile.sh
+++ src/compile.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 #Don't know cmake so here's a simple bash script for compiling
-g++ -c Directory.cpp -o Directory.o
-g++ -c Functions.cpp -o Functions.o
+g++ -std=c++11 -c Directory.cpp -o Directory.o
+g++ -std=c++11 -c Functions.cpp -o Functions.o
 g++ Functions.o Directory.o Main.cpp -o File-viewer
